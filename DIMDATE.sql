2433482225e6b9be17d8b4daf4147c2872f51eca
sandro devdariani <59333110+sandrosandro223@users.noreply.github.com>
2024-05-16 13:37:37 +0400

    Add files via upload
    The link of database which i am using - AdventureWorksDW2022.bak
    https://learn.microsoft.com/en-us/sql/samples/adventureworks-install-configure?view=sql-server-ver16&tabs=ssms

---
+++ DIMDATE.sql
@@ -0,0 +1,17 @@
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  
+  [EnglishDayNameOfWeek] AS Day, 
+ 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,   
+ 
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year 
+  
+FROM 
+ [AdventureWorksDW2022].[dbo].[DimDate]
+WHERE 
+  CalendarYear >= 2012
